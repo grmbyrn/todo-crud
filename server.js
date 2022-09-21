d2aa07f7c9a6771e3567423563e2f69d112ec470
grmbyrn <graemebyrne_89@hotmail.com>
2022-09-21 20:21:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+//Declare Variables
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+//const TodoTask = require("./models/TodoTask");
+const homeRoutes = require("./routes/home")
+const editRoutes = require("./routes/edit")
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+//Set Middleware
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+//Set Routes
+app.use('/', homeRoutes)
+app.use('/edit', editRoutes)
+
+// START SERVER
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
